22884ce38d27be671e701a989ba6ca01bb29b718
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-04-05 19:02:03 +0530

    Calculation of length

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Line_Comparison_Problems
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of (x1,y1),(x2,y2) cordinates");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(toFindLength(x1, y1, x2, y2));
+
+        }
+
+        private static int toFindLength(int x1, int y1, int x2, int y2)
+        {
+            return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+    }
+}
